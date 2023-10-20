f7fbf99d5b95e30520a79eb2d701683570db10db
patrickhum <jirapat_na@cmu.ac.th>
2023-10-20 18:02:25 +0700

    setup Admin Dashboard

---
+++ src/views/AdDashboardView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col items-center justify-center bg-white relative">
+        <div class="w-full max-w-screen-md">
+            <h1 class="text-5xl font-bold text-blue-500 mb-4 text-center">Admin Dashboard</h1>
+        </div>
+        <div class="w-full max-w-screen-md">
+            <h1 class="text-5xl font-bold text-blue-500 mb-4 text-center">Create Teacher Information</h1>
+        </div>
+        <div class="w-full max-w-screen-md">
+            <h1 class="text-5xl font-bold text-blue-500 mb-4 text-center">Edit Teacher Information</h1>
+        </div>
+        <div class="w-full max-w-screen-md">
+            <h1 class="text-5xl font-bold text-blue-500 mb-4 text-center">Overall</h1>
+        </div>
+    </div>
+</template>
