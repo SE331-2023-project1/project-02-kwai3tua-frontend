e8038f3ddaa32a49d5aa278edb004a83dcf1f267
patrickhum <jirapat_na@cmu.ac.th>
2023-10-20 23:11:13 +0700

    Add Update Student view

---
+++ src/views/UpdateTeacherView.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col items-center justify-center bg-white relative">
+        <div class="w-full max-w-screen-md">
+            <h1 class="text-5xl font-bold text-blue-500 mb-4 text-center">Update Teacher</h1>
+            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                        Name
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="name" type="text" placeholder="Name" />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="surname">
+                        Surname
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="surname" type="text" placeholder="Surname" />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="position">
+                        Academic Position
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="position" type="text" placeholder="Academic Position" />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="Department">
+                        Department
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="Department" type="text" placeholder="Department" />
+                </div>
+
+                <!-- Add the profile image input -->
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="profileImage">
+                        Profile Image
+                    </label>
+                    <input type="file"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="profileImage" accept="image/*" />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="username" type="text" placeholder="Username" />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password" type="text" placeholder="Password" />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="advisees">
+                        Advisees
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="advisees" type="text" placeholder="Advisees" />
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <RouterLink to="/Addashboard"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        Update Teacher
+                    </RouterLink>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
