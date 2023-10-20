11545a85a2bedcd69ce41c1ab8dc6ee3feea7efc
patrickhum <jirapat_na@cmu.ac.th>
2023-10-20 17:08:56 +0700

    change button to routerlink

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -23,11 +23,12 @@
             id="password" type="password" placeholder="Password" />
         </div>
         <div class="flex items-center justify-between">
-          <button
+          <RouterLink
+            to="/student" 
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button">
+          >
             Sign In
-          </button>
+          </RouterLink>
           <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
             Student Register
           </a>
