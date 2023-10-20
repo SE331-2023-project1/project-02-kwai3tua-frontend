f3b03c4bd47f31128cf281f8164abd1dada33ba7
ptpofficialxd <aekkungzaza@gmail.com>
2023-10-20 18:51:10 +0700

    add AdvisorDashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import StudentDashboardView from '../views/StudentDashboardView.vue'
+import AdvisorDashboardView from '../views/AdvisorDashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/student',
       name: 'studentdashboard',
       component: StudentDashboardView
+    },
+    {
+      path: '/advisor',
+      name: 'advisordashboard',
+      component: AdvisorDashboardView
     }
   ]
 })
---
+++ src/views/AdvisorDashboardView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AnnouncementCard from '@/components/AnnouncementCard.vue'
+import StudentListCard from '@/components/StudentListCard.vue'
+import StudentCommentCard from '@/components/StudentCommentCard.vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-start">
+    <h1 class="font-bold text-4xl text-blue-500 mt-6">Advisor Dashboard</h1>
+  </div>
+  <div class="ml-10 mr-10">
+    <h1 class="font-bold text-2xl text-blue-500">Post Annoucement</h1>
+    <AnnouncementCard></AnnouncementCard>
+  </div>
+  <div class="flex">
+    <div class="ml-10 mr-10">
+      <h1 class="font-bold text-2xl text-blue-500">Advisee List</h1>
+      <StudentListCard></StudentListCard>
+    </div>
+    <div class="mr-10">
+      <h1 class="font-bold text-2xl text-blue-500">Advisee Comment</h1>
+      <StudentCommentCard></StudentCommentCard>
+    </div>
+  </div>
+</template>
