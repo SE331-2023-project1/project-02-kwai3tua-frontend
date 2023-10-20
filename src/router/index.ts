fdcac10fd030c4d828800dfeb72edbea06b3f8b6
patrickhum <jirapat_na@cmu.ac.th>
2023-10-20 17:39:06 +0700

    Student information page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import AdLoginView from '../views/AdLoginView.vue'
+import StudentInfoView from '../views/StudentInfoView.vue'
 import StudentDashboardView from '../views/StudentDashboardView.vue'
 
 const router = createRouter({
@@ -21,6 +22,11 @@ const router = createRouter({
       path: '/Adlogin',
       name: 'adminlogin',
       component: AdLoginView
+    },
+    {
+      path: '/StudentInfo',
+      name: 'studentinfo',
+      component: StudentInfoView
     },
     {
       path: '/student',
