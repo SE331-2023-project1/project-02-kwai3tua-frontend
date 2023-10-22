32827f6bf1547df084626412b63da9f245447204
ptpofficialxd <aekkungzaza@gmail.com>
2023-10-23 01:47:02 +0700

    add create announcement for advisor and update responsiveness

--- src/views/AdvisorDashboardView.vue
+++ src/views/AdvisorDashboardView.vue
@@ -36,29 +36,73 @@ watchEffect(() => {
     })
 })
 
+const announcementText = ref(''); // Define a ref for announcement text
+
+const postAnnouncement = () => {
+  // Handle posting the announcement here
+  // You can use announcementText.value to access the announcement text
+  // and you can handle file uploads here
+}
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-start">
     <h1 class="font-bold text-5xl text-blue-500 mt-6 mb-6">Advisor Dashboard</h1>
   </div>
-  <div class="">
-    <h1 class="font-bold text-2xl text-blue-500">Post Annoucement</h1>
-    <AnnouncementCard></AnnouncementCard>
-  </div>
-  <div class="">
-    <div class="">
-      <h1 class="font-bold text-2xl text-blue-500 mb-5">Advisee List</h1>
+  <hr class="border-2 border-black rounded"/>
+  <h1 class="pt-10 font-bold text-2xl text-blue-500 text-center">Create Announcement</h1>
+    <div class="mt-5 mb-5 border-2 border-black rounded-lg py-4">
+      <form @submit.prevent="postAnnouncement">
+        <div class="mb-4">
+          <label for="announcement" class="px-4 block text-gray-700 text-xl font-bold mb-2">What are you announcing?</label>
+          <textarea
+            id="announcement"
+            v-model="announcementText"
+            class="announcebox h-24 p-2 border rounded focus:outline-none focus:shadow-outline"
+            placeholder="Enter your announcement message here"
+          ></textarea>
+        </div>
+        <div class="mb-4">
+          <label for="file" class="px-4 block text-gray-700 text-xl font-bold mb-2">Upload File (optional)</label>
+          <input
+            type="file"
+            id="file"
+            class="announcebox p-2 border rounded focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div class="flex justify-end">
+          <button class="mr-4 bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline">
+            Post Announcement
+          </button>
+        </div>
+      </form>
+    </div>
+  <div>
     <div>
+      <h1 class="font-bold text-2xl text-blue-500 mb-5 text-center">Advisee List</h1>
+      <div>
       <RouterLink v-for="student in studentList" 
       :key="student.studentId" :to="{ name: 'studentdetail', params: { studentId: student.studentId } }">
       <StudentListCard :student="student"></StudentListCard>
       </RouterLink>
     </div>
-    </div>
-    <div class="">
-      <h1 class="font-bold text-2xl text-blue-500">Advisee Comment</h1>
+  </div>
+    <div>
+      <h1 class="font-bold text-2xl text-blue-500 mt-5 text-center">Advisee Comment</h1>
       <StudentCommentCard></StudentCommentCard>
     </div>
   </div>
 </template>
+
+<style scoped>
+hr {
+  width: 250px;
+  margin: auto; 
+  display: block;
+}
+.announcebox {
+  width: 990px;
+  margin: auto; 
+  display: block;
+}
+</style>
--- src/views/StudentDashboardView.vue
+++ src/views/StudentDashboardView.vue
@@ -8,18 +8,27 @@ import AdvisorCommentCard from '@/components/AdvisorCommentCard.vue'
   <div class="flex flex-col items-center justify-start">
     <h1 class="font-bold text-5xl text-blue-500 mt-6 mb-6">Student Dashboard</h1>
   </div>
-  <div class="">
-    <h1 class="font-bold text-2xl text-blue-500">Annoucement</h1>
+  <hr class="border-2 border-black rounded"/>
+  <div>
+    <h1 class="pt-10 font-bold text-2xl text-blue-500 text-center">Annoucement</h1>
     <AnnouncementCard></AnnouncementCard>
   </div>
-  <div class="">
-    <div class="">
-      <h1 class="font-bold text-2xl text-blue-500">Advisor Detail</h1>
+  <div>
+    <div>
+      <h1 class="font-bold text-2xl text-blue-500 text-center">Advisor Detail</h1>
       <AdvisorDetailCard></AdvisorDetailCard>
     </div>
-    <div class="">
-      <h1 class="font-bold text-2xl text-blue-500">Advisor Comment</h1>
+    <div>
+      <h1 class="font-bold text-2xl text-blue-500 text-center">Advisor Comment</h1>
       <AdvisorCommentCard></AdvisorCommentCard>
     </div>
   </div>
 </template>
+
+<style scoped>
+hr {
+  width: 250px;
+  margin: auto; 
+  display: block;
+}
+</style>
