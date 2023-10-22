7c9588aed227ea9e5c6e8d3379361e30d26957f1
Yotsawat <yotsawat_l@cmu.ac.th>
2023-10-22 22:58:02 +0700

    Make UploadImg

--- src/views/StudentInfoView.vue
+++ src/views/StudentInfoView.vue
@@ -4,6 +4,8 @@ import { ref } from 'vue'
 import type { StudentItem } from '@/type';
 import StudentService from '@/services/StudentService'
 import { useRouter } from 'vue-router';
+import type ImageUpload from '@/components/ImageUpload.vue';
+import BaseInput from '@/components/BaseInput.vue'
 
 const router = useRouter()
 const student = ref<StudentItem>({
@@ -14,7 +16,7 @@ id: 0,
 studentId: 0,
 name: '',
 surname: '',
-profileImg: '',
+profileImg: [],
 department: '',
 advisor: {id: 0, name:''}
 }
@@ -38,46 +40,55 @@ function saveStudent() {
             <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <form @submit.prevent="saveStudent" >
                 <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="StudentID">
                         Student ID
                     </label>
-                    <input
+                    <BaseInput
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        v-model="student.studentId" id="StudentId" type="text" placeholder="Name" />
+                        v-model="student.studentId" id="StudentId" type="text"  />
                 </div>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
                         Name
                     </label>
-                    <input
+                    <BaseInput
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        v-model="student.name" id="name" type="text" placeholder="Name" />
+                        v-model="student.name" id="name" type="text"  />
                 </div>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="surname">
                         Surname
                     </label>
-                    <input
+                    <BaseInput
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        v-model="student.surname" id="surname" type="text" placeholder="Surname" />
+                        v-model="student.surname" id="surname" type="text" />
                 </div>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="Department">
                         Department
                     </label>
-                    <input
+                    <BaseInput
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        v-model="student.department" id="Department" type="text" placeholder="Department" />
+                        v-model="student.department" id="Department" type="text" />
                 </div>
+                <!-- <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="Advisor">
+                        Advisor
+                    </label>
+                    <BaseInput
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        v-model="student.advisor" id="Advisor" type="text" />
+                </div> -->
                 <!-- Add the profile image input -->
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="profileImage">
                         Profile Image
                     </label>
-                    <input type="text"
+                    <!-- <input type="file"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        v-model="student.profileImg" id="profileImage" accept="image/*" />
-                </div>
+                        v-model="student.profileImg" id="profileImage" accept="image/*" /> -->
+                        <ImageUpload v-model="student.profileImg" />
+                    </div>
                 <div class="flex items-center justify-between">
                     <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"> 
                        Create Profile               
